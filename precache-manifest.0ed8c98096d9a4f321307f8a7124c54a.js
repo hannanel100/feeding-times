796dc2cb40e5493803b89dca2b4a8e460c6b10dd
hannanel gershinsky <hannanel100@gmail.com>
2020-10-18 18:38:08 +0300

    Updates

---
+++ precache-manifest.0ed8c98096d9a4f321307f8a7124c54a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85952122970ab513249108ad67989175",
+    "url": "/feeding-times/index.html"
+  },
+  {
+    "revision": "5863ce42e2b21df2e960",
+    "url": "/feeding-times/static/css/main.08959912.chunk.css"
+  },
+  {
+    "revision": "2a1a6efb3ac3b65c3600",
+    "url": "/feeding-times/static/js/2.03ac572f.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/feeding-times/static/js/2.03ac572f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5863ce42e2b21df2e960",
+    "url": "/feeding-times/static/js/main.03f12d08.chunk.js"
+  },
+  {
+    "revision": "c72cccd45b1968f1fe5f",
+    "url": "/feeding-times/static/js/runtime-main.498dbbb8.js"
+  }
+]);
