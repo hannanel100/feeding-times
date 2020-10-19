6bfe18463e8de12dc74a1cf0db8e93d7bbe513ce
hannanel gershinsky <hannanel100@gmail.com>
2020-10-19 15:41:13 +0300

    working on refactoring to firebase hooks

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState, createContext, useReducer } from "react";
 import firebase from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const AuthContext = createContext({});
 const { Provider } = AuthContext;
 
 export const AuthProvider = ({ children }) => {
+  // const [user, loading, error] = useAuthState(firebase.auth());
   const [user, setUser] = useState(null);
   const [loadingAuthState, setLoadingAuthState] = useState(true);
   useEffect(() => {
